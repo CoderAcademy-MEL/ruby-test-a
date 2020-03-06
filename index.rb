2d3c0a505dd52b9f484fbb9d43d3bc604c7ab438
M3rgos <serge@terreu.com>
2020-03-06 15:21:42 +1100

    challenge completed

--- index.rb
+++ index.rb
@@ -1,9 +1,9 @@
 # Question 1: What is a variable?
-
+# Variable can hold data types to it  
 # Question 2: What Ruby method can you use to make a loop from an array?
-
+# using while  
 # Question 3: In Ruby, what are the booleans?
-
+# they're used for true or false statement 
 # Question 4: 
 
 # a. In Ruby what is coercion? Explain what coercion is using the code snippet below.
@@ -12,38 +12,55 @@
 #   puts "hi"
 # end 
 
+# cant explain it
+
 # Question 5: What is a class? 
-
+# its the blueprint of a certain object (e.g lion) 
 # Question 6: What is an instance variable?
-
+# its an property/attribute of a class ('the mane' - on a lion is a instance variable of the class lion 
 # Question 7: What is handy about using gems in Ruby?
-
+#trying out different 'unique' functions inside gems
 # Question 8:  Use string interpolation and the variable my_string = “Fred” to print to output the string “My name is Fred”
-
+# p "My name is" + my_string  
 # Question 9: Give one method to remove whitespace from user input in Ruby
-
+#user_input.strip 
 # Question 10: What method would you use to get input from a user?
-
+# gets 
 # Question 11: What result would you get in pry or IRB for this equation: 2 + 7 % 3 ? Remember the order of operations!
-
+#0
 # Question 12: Name one method you could use to access the last element in an array
-
+# .index[] 
 # Question 13: What the method do you use to create new objects (or instances of a class)?
-
+#initialize
 # Question 14: 
 
 # a. Define a class called fish.
-
+#def class Fish
 # b. Add an initialize method, it should contain two attributes, name and species, they should be accessible for reading and writing
-
+# def initialize
+#    @name = name
+#    @species = species
+#end
 # c. Create one fish object (nemo) and store it in a variable, it should contain the correct number of arguments that will be used for nemo's attributes
-
+# nemo.new = name (not sure beyond)
 # Question 15: Define a hash called ‘student’. This hash should have two keys, one called name, and the other called age, and assign a string to the name key, and an integer to the age key. 
-
+# def student {name: serge, age: 31
+#}
+#  student_name = 'name' 
+#  student_age = age 
+#end 
 # Question 16: Write a Ruby method that takes three arguments and returns the sum (total) of those three arguments.
+#def math(a,b,c)
+  #sum = math(a,b,c) 
+  #return sum=a+b+c 
+#end
+
+#math(2,3,4)
 
 # Question 17: Write a Ruby method that takes an array and a string as arguments, and returns true if the string is in the array, and false if it is not. Use a loop in this method, and do not use the inbuilt Ruby Array methods (except .each).
-
+# not sure (clueless with def methods)
 # Question 18: Write a method that takes one argument, a number, and that returns true if the number is divisible by three, and false otherwise. 
-
+# not sure (clueless with def methods)
+ 
 # Question 19: You must use your previous method in this next question. Write another method that takes an array (of numbers) as an argument. This method will use your first method to return two arrays (within one array - an array of arrays) The first array contains the numbers that are divisible by 3, and the other contains the rest of the numbers. For example, if you pass the second method the array [9, 4, 3, 5, 30, 2, 8] it would return [[9, 3, 30],[4, 5, 2, 8]].
+# not sure (clueless with def methods)
