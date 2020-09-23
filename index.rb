2fa03ef68d50cd6bf84fd8664bb910f9dd86f7ee
Clare Forrest <clare.a.forrest@gmail.com>
2020-09-23 10:23:12 +1000

    challenge completed

--- index.rb
+++ index.rb
@@ -1,49 +1,98 @@
 # Question 1: What is a variable?
+#a name given to an object in ruby
 
 # Question 2: What Ruby method can you use to make a loop from an array?
+#def loop_method(array)
+  #array.each do |array_item|
+  #end
+#end
 
 # Question 3: In Ruby, what are the booleans?
+#responses to code that are either true or false 
 
 # Question 4: 
-
 # a. In Ruby what is coercion? Explain what coercion is using the code snippet below.
-
+#coercion is the changing of one datatype to another. It can be done either implicitly or explicitly.
+#In the below example, it's explicitly coercing a nil value to a string based on a conditional if statement.
 # if nil 
 #   puts "hi"
 # end 
 
 # Question 5: What is a class? 
+#Class is an object that the developer has created. I can have its own attributes, methods and variables 
 
 # Question 6: What is an instance variable?
+#a variable (name given to an object) that relates specifically to one instance (creation) of a class 
 
 # Question 7: What is handy about using gems in Ruby?
+#It's code already written that completes something that you need. It prevents a double up on work
 
 # Question 8:  Use string interpolation and the variable my_string = “Fred” to print to output the string “My name is Fred”
+#puts "My name is #{my_string}"
 
 # Question 9: Give one method to remove whitespace from user input in Ruby
+# .strip
 
 # Question 10: What method would you use to get input from a user?
+# gets
 
 # Question 11: What result would you get in pry or IRB for this equation: 2 + 7 % 3 ? Remember the order of operations!
+#3
 
 # Question 12: Name one method you could use to access the last element in an array
+#.last 
 
 # Question 13: What the method do you use to create new objects (or instances of a class)?
+#.new
 
 # Question 14: 
-
 # a. Define a class called fish.
+#class Fish
+#end 
 
 # b. Add an initialize method, it should contain two attributes, name and species, they should be accessible for reading and writing
+#class Fish
+# attr_accessor :name :species
+#   def initialize(name, species)
+#     @name = name
+#     @species = species
+#   end 
+#end 
 
 # c. Create one fish object (nemo) and store it in a variable, it should contain the correct number of arguments that will be used for nemo's attributes
+#nemo = Fish.new("nemo", "clownfish")
 
 # Question 15: Define a hash called ‘student’. This hash should have two keys, one called name, and the other called age, and assign a string to the name key, and an integer to the age key. 
+student = {name: "string" age: 0}
 
 # Question 16: Write a Ruby method that takes three arguments and returns the sum (total) of those three arguments.
+# def sum_total(arg1, arg2, arg3)
+#   total = arg1 + arg2 + arg3
+# end 
 
-# Question 17: Write a Ruby method that takes an array and a string as arguments, and returns true if the string is in the array, and false if it is not. Use a loop in this method. You can use .each but you CANNOT use .include?
+# Question 17: Write a Ruby method that takes an array and a string as arguments, 
+#and returns true if the string is in the array, and false if it is not. 
+#Use a loop in this method. You can use .each but you CANNOT use .include?
+# alphabet = ["a", "b", "c"]
+
+# def question17(array, query)
+#   array.each do |letter| 
+#   p letter == query
+#   end
+# end 
+
+# question17(alphabet, "c")
 
 # Question 18: Write a method that takes one argument, a number, and that returns true if the number is divisible by three, and false otherwise. 
+# def question18(number)
+#   answer = number / 3
+#   answer
+# end 
 
-# Question 19: You must use your previous method in this next question. Write another method that takes an array (of numbers) as an argument. This method will use your first method to return two arrays (within one array - an array of arrays) The first array contains the numbers that are divisible by 3, and the other contains the rest of the numbers. For example, if you pass the second method the array [9, 4, 3, 5, 30, 2, 8] it would return [[9, 3, 30],[4, 5, 2, 8]].
+# p question18(3)
+
+# Question 19: You must use your previous method in this next question. 
+#Write another method that takes an array (of numbers) as an argument. 
+#This method will use your first method to return two arrays (within one array - an array of arrays) 
+#The first array contains the numbers that are divisible by 3, and the other contains the rest of the numbers. 
+#For example, if you pass the second method the array [9, 4, 3, 5, 30, 2, 8] it would return [[9, 3, 30],[4, 5, 2, 8]].
